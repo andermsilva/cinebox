4499a7caec91063b06f76264b73ec4c7cf484212
andermsilva <67654030+andermsilva@users.noreply.github.com>
2025-08-15 20:30:18 -0300

    tela splash

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -15,21 +15,22 @@ class _SplashScreenState extends ConsumerState<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Screen riverpod'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          //showLoader();
-          /*  await Future.delayed(Duration(seconds: 2));
-          hideLoader();*/
-          showInfoSnackBar('Info qualquer');
-          showSuccessSnackBar('Sucesso');
-          showErrorSnackBar('Erro qualquer!');
-        },
-      ),
-      body: Center(
-        child: Image.asset(R.ASSETS_IMAGES_BG_LOGIN_PNG),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            color: Colors.black.withAlpha(170),
+          ),
+          Center(
+            child: Image.asset(R.ASSETS_IMAGES_LOGO_PNG),
+          ),
+        ],
       ),
     );
   }
