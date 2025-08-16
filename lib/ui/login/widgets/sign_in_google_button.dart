f45d97c312204b87504cf12a34a56cdf4496b1e7
andermsilva <67654030+andermsilva@users.noreply.github.com>
2025-08-16 19:26:20 -0300

    construindo tela de login

---
+++ lib/ui/login/widgets/sign_in_google_button.dart
@@ -0,0 +1,36 @@
+import 'package:cinebox/ui/core/themes/colors.dart';
+import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:flutter/material.dart';
+
+class SignInGoogleButton extends StatefulWidget {
+  const SignInGoogleButton({super.key});
+
+  @override
+  State<SignInGoogleButton> createState() => _SignInGoogleButtonState();
+}
+
+class _SignInGoogleButtonState extends State<SignInGoogleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        padding: EdgeInsets.zero,
+      ),
+      onPressed: () {},
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Image.asset(R.ASSETS_IMAGES_GOOGLE_LOGO_PNG),
+          ),
+          Text(
+            'Entrar com o Google',
+            style: TextStyle(fontSize: 14, color: AppColors.darkGrey),
+          ),
+        ],
+      ),
+    );
+  }
+}
