d8445a44b73f26cc43739380cff81d5ba7de8546
andermsilva <67654030+andermsilva@users.noreply.github.com>
2025-08-17 22:52:29 -0300

    mpvie by category command

---
+++ lib/ui/movies/commands/get_movies_by_category_command.dart
@@ -0,0 +1,29 @@
+import 'package:cinebox/core/result/result.dart';
+import 'package:cinebox/domain/models/movies_by_category.dart';
+import 'package:cinebox/domain/usecases/usecases_providers.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'get_movies_by_category_command.g.dart';
+
+@riverpod
+class GetMoviesByCategoryCommand extends _$GetMoviesByCategoryCommand {
+  @override
+  AsyncValue<MoviesByCategory?> build() {
+    return const AsyncData(null);
+  }
+
+  Future<void> execute() async {
+    state = AsyncLoading();
+
+    final getMoviesBycategoryUC = ref.read(getMoviesByCategoryUsercaseProvider);
+    final result = await getMoviesBycategoryUC.execute();
+
+    state = switch (result) {
+      Success(value: final moviesByCategory) => AsyncData(moviesByCategory),
+      Failure() => AsyncError(
+        'Erro ao buscar filme por Categoria',
+        StackTrace.current,
+      ),
+    };
+  }
+}
